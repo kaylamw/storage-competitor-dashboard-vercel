9f9f14e30c0c48052146a64205189bf0063001f9
kaylamw <52092570+kaylamw@users.noreply.github.com>
2025-06-10 20:16:17 -0700

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,4 +1,6 @@
-export default async function handler(req, res) {
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+module.exports = async function handler(req, res) {
   const { zip } = req.query;
 
   try {
@@ -17,4 +19,4 @@ export default async function handler(req, res) {
     console.error('API fetch error:', error);
     res.status(500).json({ error: 'Server error' });
   }
-}
+};

a149894ca8d1a93f9fc41b1a49d257bf829b1ced
kaylamw <52092570+kaylamw@users.noreply.github.com>
2025-06-11 20:27:41 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
@@ -7,3 +8,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
+
